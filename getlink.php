2bb4539eb17187e6fed23bd824ed70384299d191
Thrickr Gwo <ze3kr@icloud.com>
2016-02-06 09:30:16 +0800

    no message

--- getlink.php
+++ getlink.php
@@ -7,7 +7,7 @@ $explode = explode('/',substr(strstr($_GET['url'],'http://www.bilibili.com/'),24
 if( $explode[0] == 'video' ) {
 	$aid = substr($explode[1],2);
 	if(substr($explode[2],6,-5)>0) {
-		$pid = substr($explode[3],6,-5);
+		$pid = substr($explode[2],6,-5);
 	} else {
 		$pid = 1;
 	}
